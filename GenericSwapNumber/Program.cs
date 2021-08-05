9e38176f4ea6d190c0555bd13581350a274e17c0
iamgadds <husain.ghadiyali07@gmail.com>
2021-08-05 13:55:13 +0530

    first commit swap numbers

---
+++ GenericSwapNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSwapNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapNumber<int> sn = new SwapNumber<int>();
+            Console.WriteLine("Enter Number1:");
+            sn.number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Number2:");
+            sn.number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("------Swapping Numbers-------");
+            sn.swap();
+            Console.WriteLine("Number 1: " + sn.number1 + "\nNumber2: " + sn.number2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GenericSwapNumber/SwapNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSwapNumber
+{
+    public class SwapNumber<UNKNOWNDATATYPE>
+    {
+        public UNKNOWNDATATYPE number1 { get; set; }
+        public UNKNOWNDATATYPE number2 { get; set; }
+        public void swap()
+        {
+            UNKNOWNDATATYPE number3 = number1;
+            number1 = number2;
+            number2 = number3;
+        }
+    }
+}
